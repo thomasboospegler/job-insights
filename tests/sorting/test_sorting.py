28e28a7c3987abceb6a35550a1c2af6a12cfa863
Thomas Boos Pegler <tboospegler@gmail.com>
2023-04-13 18:28:31 -0400

    test: add test for sort_by function

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,36 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"max_salary": 100, "min_salary": 2, "date_posted": "2022-01-01"},
+        {"max_salary": 300, "min_salary": 1, "date_posted": "2021-01-01"},
+        {"max_salary": 200, "min_salary": 0, "date_posted": "2023-01-01"},
+    ]
+
+    sorted_by_max = [
+        {"max_salary": 300, "min_salary": 1, "date_posted": "2021-01-01"},
+        {"max_salary": 200, "min_salary": 0, "date_posted": "2023-01-01"},
+        {"max_salary": 100, "min_salary": 2, "date_posted": "2022-01-01"},
+    ]
+
+    sorted_by_min = [
+        {"max_salary": 200, "min_salary": 0, "date_posted": "2023-01-01"},
+        {"max_salary": 300, "min_salary": 1, "date_posted": "2021-01-01"},
+        {"max_salary": 100, "min_salary": 2, "date_posted": "2022-01-01"},
+    ]
+
+    sorted_by_date = [
+        {"max_salary": 200, "min_salary": 0, "date_posted": "2023-01-01"},
+        {"max_salary": 100, "min_salary": 2, "date_posted": "2022-01-01"},
+        {"max_salary": 300, "min_salary": 1, "date_posted": "2021-01-01"},
+    ]
+
+    sort_by(jobs, "max_salary")
+    assert jobs == sorted_by_max
+
+    sort_by(jobs, "min_salary")
+    assert jobs == sorted_by_min
+
+    sort_by(jobs, "date_posted")
+    assert jobs == sorted_by_date
