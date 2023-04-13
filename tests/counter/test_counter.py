a9ad84916e2a64f70122e29896909e777d34235e
Thomas Boos Pegler <tboospegler@gmail.com>
2023-04-13 13:35:42 -0400

    test: add test for count_ocurrences function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import patch, mock_open
 
 
 def test_counter():
-    pass
+    with patch("builtins.open", mock_open(read_data="aaa aaa")):
+        assert count_ocurrences("path", "AAA") == 2
