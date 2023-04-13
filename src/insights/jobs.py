0c0728010c2b55b649008627599f0295084f7caa
Thomas Boos Pegler <tboospegler@gmail.com>
2023-04-13 11:29:46 -0400

    feat: add get_unique_job_types function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -37,7 +37,12 @@ def get_unique_job_types(path: str) -> List[str]:
     list
         List of unique job types
     """
-    raise NotImplementedError
+    jobs = read(path)
+    job_types = []
+    for job in jobs:
+        if job["job_type"] not in job_types:
+            job_types.append(job["job_type"])
+    return job_types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
