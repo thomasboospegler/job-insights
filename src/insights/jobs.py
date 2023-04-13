94e68a0af3cf3b70ed6c211f9b69569831aa3755
Thomas Boos Pegler <tboospegler@gmail.com>
2023-04-13 11:23:49 -0400

    feat: add jobs.csv reader function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
@@ -16,7 +17,9 @@ def read(path: str) -> List[Dict]:
     list
         List of rows as dicts
     """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as file:
+        file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        return list(file_reader)
 
 
 def get_unique_job_types(path: str) -> List[str]:
